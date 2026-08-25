964831560625d48ff9562fd724de855b3e0d7258
mariellacruz12 <mariellakeithanne@gmail.com>
2026-08-25 20:31:42 +0800

    Add files via upload
    code_challenge1

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+phrase1 = "\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t*\t\t\t\t\t\t*\n\t\t\t*  Hi, My Name is Mariella Keith Anne A. Cruz   *\n\t\t\t*               From BSIT-1B                    *\n\t\t\t*\t\t\t\t\t\t*\n\t\t\t*   *   *   *   *   *   *   *   *   *   *   *   *\n\t\t\t*   *   *   *   *   *   *   *   *   *   *   *   *"
+
+print(phrase1)
