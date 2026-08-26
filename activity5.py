e5262c2725b6852b0ed3942ac5af8b500205c3ee
mariellacruz12 <mariellakeithanne@gmail.com>
2026-08-26 11:10:20 +0800

    Add files via upload
    activity5

---
+++ activity5.py
@@ -0,0 +1,17 @@
+#operators activity
+
+a = 20
+b = 8
+sum = a + b
+diff = a - b
+multiply = a * b
+divide = a / b
+modulus = a % b
+exponent = a ** b
+
+print("The sum of ",a," and", b," is",sum)
+print("The difference of ",a," and", b," is",diff)
+print("The product of ",a," and", b," is",multiply)
+print("The dividend of ",a," and", b," is",divide)
+print("The modulus of ",a," and", b," is",modulus)
+print("The exponent of ",a," and", b," is",exponent)
