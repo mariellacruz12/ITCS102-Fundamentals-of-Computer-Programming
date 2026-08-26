e2c6c4a48b96ae9a1b15db797b55ac46ace7ce88
mariellacruz12 <mariellakeithanne@gmail.com>
2026-08-26 10:11:08 +0800

    Add files via upload
    activity3

---
+++ activity3.py
@@ -0,0 +1,38 @@
+#using variables
+
+#int
+age = 22
+height = 164
+weight = 60
+birthdate = 12
+
+#float
+temp = 35.6
+speed = 12.8
+
+
+#string
+username = "123ikawlangsapatna"
+password = "mahalparinkita"
+
+#boolean
+isHungry = True
+isThief = False
+
+print("My age is", age)
+
+print("My height is", height)
+
+print("My temperature is", temp)
+
+print("My weight is", weight)
+
+print("My birthdate is",birthdate)
+
+print("My username is", username)
+
+print("My password is", password)
+
+print("Is Jeorge hungry?", isHungry)
+
+print("Is Mae the thief?", isThief)
