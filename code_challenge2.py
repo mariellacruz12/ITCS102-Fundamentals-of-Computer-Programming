7d44062885d4d1471859b79e98fc4f1c3014fa42
mariellacruz12 <mariellakeithanne@gmail.com>
2026-08-29 19:05:57 +0800

    Add files via upload
    code_challenge2

---
+++ code_challenge2.py
@@ -0,0 +1,53 @@
+money = 19863
+
+print("Money to Deposit -->", money)
+
+#computation here
+
+a = 19863
+b = 1000
+result = a // b
+
+print("The total of ",b," in", a," is",result)
+
+a = 19863
+b = 500
+c = 863
+result = c // b
+
+print("The total of ",b," in", a," is",result)
+
+a = 19863
+b = 200
+c = 363
+result = c // b
+
+print("The total of ",b," in", a," is",result)
+
+a = 19863
+b = 100
+c = 163
+result = c // b
+
+print("The total of ",b," in", a," is",result)
+
+a = 19863
+b = 50
+c = 63
+result = c // b
+
+print("The total of ",b," in", a," is",result)
+
+a = 19863
+b = 10
+c = 13
+result = c // b
+
+print("The total of ",b," in", a," is",result)
+
+a = 19863
+b = 1
+c = 3
+result = c // b
+
+print("The total of ",b," in", a," is",result)
