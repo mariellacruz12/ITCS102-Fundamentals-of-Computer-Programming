7a70100f841c7d8678fb6e0542ad4015fea1477e
Mariella Keith Anne A. Cruz <mariellakeithanne@gmail.com>
2026-09-07 10:55:54 +0800

    Add files via upload
    activity9

---
+++ activity9.py
@@ -0,0 +1,22 @@
+#relational operations
+
+x = 20
+y = 40
+
+a = "red"
+b = "blue"
+
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+print(x==y)
+print(x!=y)
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
