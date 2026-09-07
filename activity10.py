bf089347c907209600942f32cdc367758662c0cb
Mariella Keith Anne A. Cruz <mariellakeithanne@gmail.com>
2026-09-07 11:11:13 +0800

    Add files via upload
    activity10

---
+++ activity10.py
@@ -0,0 +1,13 @@
+#basic if else program
+
+username = "oopsoops"
+password = "ayoko_na"
+
+u = input("Input USERNAME ---> ")
+p = input("Input PASSWORD ---> ")
+
+if u == username or p == password :
+        print("username and password correct")
+
+else:
+        print("access denied")
