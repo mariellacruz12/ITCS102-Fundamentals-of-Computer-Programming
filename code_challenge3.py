3d5665fddb0ef8ee17899a0b563c7075be1dcd6c
Mariella Keith Anne A. Cruz <mariellakeithanne@gmail.com>
2026-09-11 16:29:20 +0800

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -16,24 +16,27 @@ base_cost = (weight * 2.50) + (distance * 0.15)
 
 #Pricing Tiers
 
-if weight is <= 2.0 distance <= 100, and not is_express or not is_international : 
-          total = 0.00
+if weight <= 2.0 and distance <= 100 and not is_express and not is_international:
+    total = 0.00
 
-elif is_express and is_international :
-          total = (base_cost * 1.40 ) + 50
+elif is_international and is_express:
+    total = (base_cost * 1.40) + 50
 
-elif is_express or (is_international and weight > 20 ) : 
-          total = (base_cost * 1.20) + 25
+elif is_express or (is_international and weight > 20):
+    total = (base_cost * 1.20) + 25
 
-elif weight > 30 or distance > 1000 :
-          total = base_cost + 30
+elif weight > 30 or distance > 1000:
+    total = base_cost + 30
 
-else :
-          total = base_cost
+else:
+    total = base_cost
+
 
 print("========== SHIPPING COST ==========")
-print("Sender:", sender)
+print("Sender Name:", sender)
+print("Item Name:", type_of_item)
+print("Item weight:", weight)
+print("Base Cost:", base_cost)
 print("TOTAL COST: $", total)
-
 
 
