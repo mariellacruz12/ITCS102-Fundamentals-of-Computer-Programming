dd597b1f4e2d5de577751eb368f8bb08e5dd9e8a
Mariella Keith Anne A. Cruz <mariellakeithanne@gmail.com>
2026-09-14 11:28:19 +0800

    Add files via upload
    activity13

---
+++ activity13.py
@@ -0,0 +1,21 @@
+age=int(input("Enter Age --->"))
+is_employed=bool(input("Are You currently employed? --->"))
+credit_score=eval(input("Credit Score History --->"))
+annual_income=eval(input("How much is your income? --->"))
+has_collateral=bool(input("Do you have any collateral? --->"))
+
+#TIER 1
+
+if age >= and is_employed == True:
+    if credit_score 750:
+      if has_collateral >= 100,000 == True:
+           total = pass
+       else:
+          pass
+else:
+    print("Approved")
+
+
+#TIER 2
+
+if age >
